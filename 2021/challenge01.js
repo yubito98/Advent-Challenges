9c7c3809a6d6d5983290baf9051a9f9343e50a92
Yubor Molina <yubor@inboundav.com>
2023-01-27 15:18:04 -0500

    Start project

--- 2021/challenge01.js
+++ 2021/challenge01.js
@@ -1,4 +1,20 @@
 function contarOvejas(ovejas) {
-    // aquí tu magia
-    return ovejas
+    const filtersheep = ovejas.filter(item =>{
+        item.name = item.name.toLowerCase();
+        if(item.name.includes("na") && item.color == "rojo"){
+            return item
+        }
+    }) 
+    return filtersheep
   }
+
+  const ovejas = [
+    { name: 'Noa', color: 'azul' },
+    { name: 'Euge', color: 'rojo' },
+    { name: 'Navidad', color: 'rojo' },
+    { name: 'Ki Na Ma', color: 'rojo'},
+    { name: 'AAAAAaaaaa', color: 'rojo' },
+    { name: 'Nnnnnnnn', color: 'rojo'}
+  ]
+
+  console.log(contarOvejas(ovejas))
